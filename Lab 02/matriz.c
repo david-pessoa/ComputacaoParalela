33d2bee01f6c65dcc62f5f8bbbd202e707fa0119
Cloud User <ec2-user@ip-172-31-40-61.ec2.internal>
2024-03-06 23:23:52 +0000

    matriz

--- Lab 02/matriz.c
+++ Lab 02/matriz.c
@@ -1,14 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
-#include  <pthread.h>
+#include <pthread.h>
 
 int main()
 {
 int A = {{1, 2, 3, 4, 5, 6, 7, 8},
 	 {4, 5, 6, 1, 9, 0, 2, 7},
-	 {6, 2, 3, 9, 1, 4, 0, 5}}
+	 {6, 2, 3, 9, 1, 4, 0, 5}};
 int B = {{6},
 	 {5},
-	 {2}}
+	 {2},
+	 {1},
+	 {0},
+	 {8},
+	 {4},
+	 {9}};
+int m = 3, n = 8;
 
 return 0;
 }
