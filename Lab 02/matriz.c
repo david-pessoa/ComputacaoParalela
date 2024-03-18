561a7b5118e781e949cf63bd140c6522a34c5ad0
Cloud User <ec2-user@ip-172-31-9-38.ec2.internal>
2024-03-18 23:27:28 +0000

    comentando

--- Lab 02/matriz.c
+++ Lab 02/matriz.c
@@ -7,7 +7,7 @@
 #define m 24 //Número de linhas da matriz A
 #define n 8 //Número de colunas de A e linhas de B
 
-int A[m][n] = {
+int A[m][n] = { //Matriz A
         { 1, 2, 3, 4, 5, 6, 7, 8 },
         { 9, 10, 11, 12, 13, 14, 15, 16 },
         { 17, 18, 19, 20, 21, 22, 23, 24 },
@@ -43,44 +43,43 @@ int B[][1] = {{6}, //Matriz B
 	 {4},
 	 {9}};
 
-struct ThreadArgs //struct contendo o vetor C resultante e o indíce de um dos elementos do vetor
+struct ThreadArgs //struct contendo:
 {
-    int* C;
-    int index;
-	int num_threads;
-	sem_t* semaphore;
+    int* C; //Vetor C resultante
+    int index; //Índice da thread
+	int num_threads; //Número total de threads executando
+	sem_t* semaphore; //Semáforo
 };
 
-//Função multiplica() faz com que cada thread realize o cálculo do valor do item C[k]
+//Função multiplica() faz com que cada thread realize o cálculo dos itens do vetor C
 void* multiplica(void *args)
 {
-	struct ThreadArgs* t_args = (struct ThreadArgs*)args; //extrai o vetor C e o índice k do parâmetro args passado
+	struct ThreadArgs* t_args = (struct ThreadArgs*)args; //realiza o casting e extrai os valores passados para args
     int k = t_args->index;
     int* vetor = t_args->C;
 	int threads = t_args->num_threads;
 	sem_t* semaphore = t_args->semaphore;
-	printf("Threads: %d\n", threads);
-    for(int j = 0; j < m/threads; j++)
+	
+    for(int j = 0; j < m/threads; j++) //Cada thread é responsável por calcular m/threads elementos de C
 	{
 		int valor = 0;
-		for(int i = 0; i < n; i++) //Realiza o cálculo do item C[k]
+		for(int i = 0; i < n; i++) //Realiza o cálculo do item C[(m/threads) * k + j]
 		{
 			valor += A[(m/threads) * k + j][i] * B[i][0];
 		}
-		sem_wait(semaphore);
+		sem_wait(semaphore); //Espera outra thread terminar para acessar a área de risco 
 		vetor[(m/threads) * k + j] = valor; //altera valor no vetor
-		sem_post(semaphore); 
-		printf("%d index: %d k: %d j: %d\n", valor, (m/threads) * k + j, k, j);
+		sem_post(semaphore); //Libera o semáforo
 	}
 	pthread_exit(NULL); // encerra a thread
 	
 }
-void multiplica_serial(int vetor[])
+void multiplica_serial(int vetor[]) //Função realiza a multiplicação de matrizes de maneira serial
 {
 	for(int j = 0; j < m; j++)
 	{
 		int valor = 0;
-		for(int i = 0; i < n; i++) //Realiza o cálculo do item C[k]
+		for(int i = 0; i < n; i++) //Realiza o cálculo do item C[j]
 		{
 			valor += A[j][i] * B[i][0];
 		}
@@ -109,36 +108,36 @@ void show_vetor(int vetor[], int k) // Função para exibir todos os elementos d
 int main()
 {
 	pthread_t threads[m]; //Vetor de threads
-	int C_serial[m]; //Matriz C resultante
-	int C_paralela[m];
+	int C_serial[m]; //Vetor C resultante da execução serial
+	int C_paralela[m]; //Vetor C resultante da execução em paralelo
 	zera_vetor(C_serial, m);
 	struct ThreadArgs args[m]; //args será passado como argumento para a thread
 
-	clock_t start, end;
+	clock_t start, end; //Variáveis para contar o tempo de início o fim da execução
     double serial_time, parallel_time;
-	sem_t semaphore;
+	sem_t semaphore; //Inicia semáforo
     sem_init(&semaphore, 0, 1);
 
-	start = clock();
-	multiplica_serial(C_serial);
-	end = clock();
-	serial_time = ((double)(end - start)) / CLOCKS_PER_SEC;
-	printf("Tempo de execução serial: %lfms\n", serial_time * 1000);
-	printf("Matriz serial: ");
+	start = clock(); //Inicia contagem do tempo em serial
+	multiplica_serial(C_serial); //Executa em serial
+	end = clock(); //Encerra a contagem
+	serial_time = ((double)(end - start)) / CLOCKS_PER_SEC; //Calcula o tempo
+	printf("Tempo de execução serial: %lfms\n", serial_time * 1000); //Exibe o resultado obtido
+	printf("Vetor serial: ");
 	show_vetor(C_serial, m); //Exibe o vetor C resultante
 
-	for(int i = 2; i <= n; i += 2)
+	for(int i = 2; i <= n; i += 2) //Para cada número de núcleos de processador:
 	{
 		int num_threads = i;
 		zera_vetor(C_paralela, m);
-		start = clock();
-		for(int j = 0; j < num_threads; j++) //Para cada linha da matriz A...
+		start = clock(); //Inicia contagem do tempo em paralelo
+		for(int j = 0; j < num_threads; j++) //Para cada thread a ser criada:
 		{
-			args[j].C = C_paralela;
+			args[j].C = C_paralela; //Passa informações como vetor C, número de threads, índice da thread e o semáforo para a thread
 			args[j].num_threads = num_threads;
 			args[j].index = j;
 			args[j].semaphore = &semaphore;
-			//Cria uma thread que executará o cálculo: C[m] = ∑(A[m][i] * B[i][0])
+			//Cria uma thread que fará o cálculo de m/num_threads itens de C
 			pthread_create(&threads[j], NULL, &multiplica, (void*)&args[j]);
 		}
 		//Aguarda todas as threads encerrarem usando pthread_join()
@@ -146,13 +145,13 @@ int main()
 		{
 			pthread_join(threads[j], NULL);
 		}
-		end = clock();
-        parallel_time = ((double)(end - start)) / CLOCKS_PER_SEC;
-		printf("Tempo de execução em paralelo com %d processadores: %lfms\n", num_threads, parallel_time * 1000);
-		printf("Matriz paralela: ");
+		end = clock(); //Encerra a contagem do tempo em paralelo
+        parallel_time = ((double)(end - start)) / CLOCKS_PER_SEC; //Calcula o tempo
+		printf("Tempo de execução em paralelo com %d processadores: %lfms\n", num_threads, parallel_time * 1000); //Exibe o resultado obtido
+		printf("Vetor paralelo: ");
 		show_vetor(C_paralela, m); //Exibe o vetor C resultante
 	}
-	sem_destroy(&semaphore);
+	sem_destroy(&semaphore); //Destrói o semáforo
 
 return 0;
 }
