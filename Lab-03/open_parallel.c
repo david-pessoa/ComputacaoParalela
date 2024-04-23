8e350123208aa154be167ceb264250b804203e66
Ubuntu <ubuntu@ip-172-31-26-59.ec2.internal>
2024-04-23 17:39:12 +0000

    tenta no vscode por enquanto

--- Lab-03/open_parallel.c
+++ Lab-03/open_parallel.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <math.h>
 #include <semaphore.h>
 
+#ifdef _OPENMP
+# include <omp.h>
+#endif 
+
 #define PI 3.1415
 #define COMPRIMENTO 24 * PI
+#define TRAPZ_NUM 8
+#define ALT_TRAPZ TRAPZ_NUM / COMPRIMENTO
 // Calcular a área para comprimento 24π
 // Função f(x) = sen(x) + 2
 
@@ -23,34 +28,35 @@ void AreaThread();
 int main(int argc, char* argv[])
 {
     int num_threads = strtol(argv[1], NULL, 10);
-    double ColAnterior;
-    double ColAtual = 0;
-
-    double AltAnterior;
-    double AltAtual = func(ColAtual);
-    const double COMP_THREAD = COMPRIMENTO / num_threads;
 
 # pragma omp parallel num_threads(thread_count)
 AreaThread();
     
-
     printf("A área total aproximada é: %lf", Area);
     return 0;
 }
 
 void AreaThread()
 {
+#ifdef _OPENMP
   int thread_id = omp_get_thread_num();
-  const double COMP_THREAD = COMPRIMENTO / omp_get_num_threads();  
+  const int  TRAPZ_POR_THREAD = TRAPZ_NUM / omp_get_num_threads();  
 
-  double coluna_anterior = thread_id * COMP_THREAD;
+#else
+thread_id = 0;
+const int TRAPZ_POR_THREAD = TRAPZ_NUM;
+#endif
+ for(int i = 0; i < TRAPZ_POR_THREAD; i++)
+ {
+  double coluna_anterior = thread_id * ALT_TRAPZ;
   double coluna_atual = coluna_anterior + COMP_THREAD;
   
   double altura_anterior = func(coluna_anterior);
   double altura_altual = func(coluna_atual);
-  sem_wait(semaphore);
+  sem_wait(&semaphore);
   Area += COMP_THREAD * (altura_atual + altura_anterior) / 2;
-  sem_post(semaphore);
+  sem_post(&semaphore);
+ }
 }
 sem_destroy(&semaphore);
 
