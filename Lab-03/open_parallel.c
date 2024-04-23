b27ae27a1bb6eec57f529bf46d029838562b380e
david-pessoa <pessoa.dvlbp@gmail.com>
2024-04-23 15:15:00 -0300

    semipronto, falta testar com open threads no ubuntu

--- Lab-03/open_parallel.c
+++ Lab-03/open_parallel.c
@@ -10,13 +10,11 @@
 #define PI 3.1415
 #define COMPRIMENTO 24 * PI
 #define TRAPZ_NUM 8
-#define ALT_TRAPZ TRAPZ_NUM / COMPRIMENTO
 // Calcular a área para comprimento 24π
 // Função f(x) = sen(x) + 2
 
 double Area = 0;
 sem_t semaphore;
-sem_init(&semaphore, 0, 1);
 
 double func(double x)
 {
@@ -27,36 +25,36 @@ void AreaThread();
 // comprimento p/ cada thread = 24π / num_threads
 int main(int argc, char* argv[])
 {
+    sem_init(&semaphore, 0, 1);
     int num_threads = strtol(argv[1], NULL, 10);
 
 # pragma omp parallel num_threads(thread_count)
-AreaThread();
-    
+    AreaThread();
+
     printf("A área total aproximada é: %lf", Area);
+    sem_destroy(&semaphore);
     return 0;
 }
 
 void AreaThread()
 {
+  const double ALT_TRAPZ = COMPRIMENTO / TRAPZ_NUM;
 #ifdef _OPENMP
   int thread_id = omp_get_thread_num();
   const int  TRAPZ_POR_THREAD = TRAPZ_NUM / omp_get_num_threads();  
 
 #else
-thread_id = 0;
-const int TRAPZ_POR_THREAD = TRAPZ_NUM;
+    int thread_id = 0;
+    const int TRAPZ_POR_THREAD = TRAPZ_NUM;
 #endif
  for(int i = 0; i < TRAPZ_POR_THREAD; i++)
  {
-  double coluna_anterior = thread_id * ALT_TRAPZ;
-  double coluna_atual = coluna_anterior + COMP_THREAD;
-  
+  double coluna_anterior = (thread_id + i) * ALT_TRAPZ;
+  double coluna_atual = coluna_anterior + ALT_TRAPZ;
   double altura_anterior = func(coluna_anterior);
   double altura_altual = func(coluna_atual);
   sem_wait(&semaphore);
-  Area += COMP_THREAD * (altura_atual + altura_anterior) / 2;
+  Area += ALT_TRAPZ * (altura_altual + altura_anterior) / 2;
   sem_post(&semaphore);
  }
 }
-sem_destroy(&semaphore);
-
