859f79cd94c37f8b05f63a53b418b147867860e4
Ubuntu <ubuntu@ip-172-31-21-162.ec2.internal>
2024-04-23 20:57:12 +0000

    add open_critical.c

--- Lab-03/open_critical.c
+++ Lab-03/open_critical.c
@@ -1,9 +1,8 @@
-//*************************************** PARA COMPILAR, ESCREVA: gcc -g -Wall -fopenmp -o open open_parallel.c -lm ******************************
+//*************************************** PARA COMPILAR, ESCREVA: gcc -g -Wall -fopenmp -o open open_critical.c -lm ******************************
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <semaphore.h>
 #include <time.h>
 
 #ifdef _OPENMP
@@ -17,7 +16,6 @@
 // Função f(x) = sen(x) + 2
 
 double Area = 0;
-sem_t semaphore;
 
 double func(double x)
 {
@@ -30,13 +28,11 @@ int main(int argc, char* argv[])
 {
     clock_t start, end; //Variáveis para contar o tempo de início o fim da execução
     start = clock();
-    sem_init(&semaphore, 0, 1);
     int thread_count = strtol(argv[1], NULL, 10);
 
 # pragma omp parallel num_threads(thread_count)
     AreaThread();
 
-    sem_destroy(&semaphore);
     end = clock();
     double exec_time = ((double)(end - start)) / CLOCKS_PER_SEC;
     exec_time *= 1000;
@@ -62,8 +58,8 @@ void AreaThread()
   double altura_anterior = func(coluna_anterior);
   double altura_altual = func(coluna_atual);
 
-  sem_wait(&semaphore);
-  Area += ALT_TRAPZ * (altura_altual + altura_anterior) / 2;
-  sem_post(&semaphore);
+  # pragma omp critical
+   Area += ALT_TRAPZ * (altura_altual + altura_anterior) / 2;
+  
  }
 }
